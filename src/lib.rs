b6f966410df0301d5f8e8eeec43007c800f69f85
Spxg <unsafe@outlook.es>
2021-10-20 00:26:10 +0800

    doc: write doc for this crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! # predicate-macros
+//! Easy to implement [predicate](https://github.com/Spxg/predicate) traits.
+//!
+//! ## Macros
+//! * add_field
+//! * BitAnd
+//! * BitOr
+//! * OpUnitTrait
+
 use proc_macro::TokenStream;
 use quote::quote;
 use quote::ToTokens;
@@ -7,6 +16,9 @@ use syn::DataEnum;
 use syn::DeriveInput;
 use syn::Variant;
 
+/// Add field `Unit(OpUnitRcType<OpUnit<Self>>)` to this enum.
+/// 
+/// Only impl for enum, other types will panic.
 #[proc_macro_attribute]
 pub fn add_field(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let mut item = parse_macro_input!(input as DeriveInput);
@@ -32,6 +44,7 @@ pub fn add_field(_attr: TokenStream, input: TokenStream) -> TokenStream {
     .into()
 }
 
+/// Impl `OpUnitTrait`.
 #[proc_macro_derive(OpUnitTrait)]
 pub fn impl_op_unit_trait(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as DeriveInput);
@@ -50,6 +63,7 @@ pub fn impl_op_unit_trait(input: TokenStream) -> TokenStream {
     .into()
 }
 
+/// Impl `BitAnd`
 #[proc_macro_derive(BitAnd)]
 pub fn impl_bit_and(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as DeriveInput);
@@ -72,6 +86,7 @@ pub fn impl_bit_and(input: TokenStream) -> TokenStream {
     .into()
 }
 
+/// Impl `BitOr`
 #[proc_macro_derive(BitOr)]
 pub fn impl_bit_or(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as DeriveInput);
